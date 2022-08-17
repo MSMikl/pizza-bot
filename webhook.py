869003465ee48217d1e3bf64b0ad974b2e1f027a
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-08-17 19:12:21 +0300

    FB bot initialized

--- webhook.py
+++ webhook.py
@@ -1,28 +1,17 @@
-import json
 import os
+import sys
+import json
+from datetime import datetime
 
-import redis
 import requests
 from dotenv import load_dotenv
 from flask import Flask, request
 
-from shop import get_auth_token, get_file_link, get_products_by_category_id, add_item_to_cart, get_cart, delete_item
+from shop import get_auth_token, get_products, get_file_link
 
 load_dotenv()
 URL = 'https://api.moltin.com'
 SHOP_TOKEN, _ = get_auth_token(URL, os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET'))
-CATEGORIES = {
-    'front_page': '853639e2-b8de-41f8-99c5-cf26496e96f9',
-    'spicy': 'd83ce07a-d60c-44f6-936b-729339db5dab',
-    'nourishing': '2fcce823-9baa-4b7f-81dc-0030375ae27e',
-    'special': 'eb6b470c-91e9-49df-9e00-95826bdffe3f',    
-}
-DATABASE = redis.Redis(
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    password=os.getenv('DB_PASS'),
-    db=0    
-)
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -38,91 +27,25 @@ def verify():
     return "Hello world", 200
 
 
-def handle_start(sender_id, message_text, postback):
-    send_menu(sender_id)
-    return "MENU_AWAITING"
-
-
-def handle_menu(sender_id, message_text, postback):
-    if postback:
-        if postback['payload'] in CATEGORIES.keys():
-            send_menu(sender_id, postback['payload'])
-            return 'MENU_AWAITING'
-        elif postback['title'] == 'Корзина':
-            cart = get_cart(SHOP_TOKEN, URL, sender_id)
-            send_cart(sender_id, cart)
-            return 'CART'
-        elif postback['title'] == 'В корзину':
-            add_item_to_cart(SHOP_TOKEN, URL, sender_id, postback['payload'], 1)
-            send_message(sender_id, "Добавили в заказ")
-            return 'MENU_AWAITING'
-    return 'MENU_AWAITING'
-
-
-def handle_cart(sender_id, message_text, postback):
-    if not postback:
-        return 'CART'
-    if postback['title'] == 'Добавить еще одну':
-        cart = add_item_to_cart(SHOP_TOKEN, URL, sender_id, postback['payload'], 1)
-        send_cart(sender_id, cart)
-        return 'CART'
-    elif postback['title'] == 'Убрать из заказа':
-        cart = delete_item(SHOP_TOKEN, URL, sender_id, postback['payload'])
-        send_cart(sender_id, cart)
-        return 'CART'
-    elif postback['title'] == 'К меню':
-        send_menu(sender_id)
-        return 'MENU_AWAITING'
-    else:
-        return 'CART'
-
-
-def handle_users_reply(sender_id, message_text, postback):
-    states_functions = {
-        'START': handle_start,
-        'MENU_AWAITING': handle_menu,
-        'CART': handle_cart,
-    }
-    recorded_state = DATABASE.get(f"fb_{sender_id}")
-    if not recorded_state or recorded_state.decode('utf-8') not in states_functions.keys():
-        user_state = "START"
-    else:
-        user_state = recorded_state.decode('utf-8')
-    if message_text == "/start":
-        user_state = "START"
-    state_handler = states_functions[user_state]
-    next_state = state_handler(sender_id, message_text, postback)
-    DATABASE.set(f"fb_{sender_id}", next_state.encode('utf-8'))
-
-
 @app.route('/', methods=['POST'])
 def webhook():
     """
     Основной вебхук, на который будут приходить сообщения от Facebook.
     """
     data = request.get_json()
-    print(data)
-    message_text=None
-    postback=None
     if data["object"] == "page":
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
                 if messaging_event.get("message"):  # someone sent us a message
                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"] # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event['message']["text"] # the message's text
-                    handle_users_reply(sender_id, message_text, postback)
-                elif messaging_event.get('postback'):
-                    sender_id = messaging_event["sender"]["id"]
-                    postback = messaging_event.get('postback')
-
-                    handle_users_reply(sender_id, message_text, postback)
+                    message_text = messaging_event["message"]["text"] # the message's text
+                    send_menu(sender_id)
     return "ok", 200
 
 
-def send_menu(recipient_id, category='front_page'):
-    products = json.loads(DATABASE.get(category))['data']
-    # get_products_by_category_id(SHOP_TOKEN, URL, CATEGORIES['category'])['data']
+def send_menu(recipient_id):
+    products = get_products(SHOP_TOKEN, URL)['data'][:5]
     http_proxy = os.environ['HTTP_PROXY']
     proxies = { 
               "http": http_proxy,
@@ -164,109 +87,17 @@ def send_menu(recipient_id, category='front_page'):
                         }
                     ] + [
                         {
-                            "title": f"{product['name']} {product['price'][0]['amount']} р.",
+                            "title": product['name'],
                             "subtitle": product['description'],
                             "image_url": get_file_link(SHOP_TOKEN, URL, product['relationships']['main_image']['data']['id']),
                             "buttons": [
                                 {
                                     "type": "postback",
-                                    "title": "В корзину",
-                                    "payload": product['sku']
+                                    "title": f"{product['price'][0]['amount']} р.",
+                                    "payload": "DEVELOPER_DEFINED_PAYLOAD"
                                 }
                             ]
                         } for product in products
-                    ] + [
-                        {
-                            "title": "Не нашли пиццу по вкусу?",
-                            "subtitle": 'Другие категории здесь',
-                            "image_url": "https://primepizza.ru/uploads/position/large_0c07c6fd5c4dcadddaf4a2f1a2c218760b20c396.jpg",
-                            "buttons":
-                                [
-                                    {
-                                        "type": "postback",
-                                        "title": "Особые",
-                                        "payload": 'special'
-                                    },
-                                    {
-                                        "type": "postback",
-                                        "title": "Сытные",
-                                        "payload": 'nourishing'                               
-                                    },
-                                    {
-                                        "type": "postback",
-                                        "title": "Острые",
-                                        "payload": 'spicy'
-                                    }
-                                ]
-                        }                        
-                    ]
-                }
-            }
-        }
-    }
-    response = requests.post("https://graph.facebook.com/v14.0/me/messages", params=params, headers=headers, json=request_content, proxies=proxies)
-    response.raise_for_status()   
-
-
-def send_cart(recipient_id, cart):
-    print(cart)
-    http_proxy = os.environ['HTTP_PROXY']
-    proxies = { 
-              "http": http_proxy,
-              "https": http_proxy,
-            }
-    params = {"access_token": os.environ["FB_PAGE_ACCESS_TOKEN"]}
-    headers = {"Content-Type": "application/json"}
-    request_content = {
-        "recipient": {
-            "id": recipient_id
-        },
-        "message": {
-            "attachment": {
-                "type": "template",
-                "payload": {
-                    "template_type": "generic",
-                    "elements": [
-                        {
-                            "title": "Ваша корзина",
-                            "image_url": "https://postium.ru/wp-content/uploads/2018/08/idealnaya-korzina-internet-magazina-1068x713.jpg",
-                            "buttons":
-                                [
-                                    {
-                                        "type": "postback",
-                                        "title": "Самомвывоз",
-                                        "payload": "Самовывоз"
-                                    },
-                                    {
-                                        "type": "postback",
-                                        "title": "Доставка",
-                                        "payload": "Доставка"                               
-                                    },
-                                    {
-                                        "type": "postback",
-                                        "title": "К меню",
-                                        "payload": "К меню"
-                                    }
-                                ]
-                        }
-                    ] + [
-                        {
-                            "title": f"{item['name']} - {item['quantity']} шт.",
-                            'subtitle': item.get('description'),
-                            'image_url': item.get('image'),
-                            'buttons': [
-                                {
-                                    "type": "postback",
-                                    "title": "Добавить еще одну",
-                                    "payload": item['sku']
-                                },
-                                {
-                                    "type": "postback",
-                                    "title": "Убрать из заказа",
-                                    "payload": item['id']
-                                }
-                            ]
-                        } for item in cart['items']
                     ]
                 }
             }
@@ -274,7 +105,9 @@ def send_cart(recipient_id, cart):
     }
     response = requests.post("https://graph.facebook.com/v14.0/me/messages", params=params, headers=headers, json=request_content, proxies=proxies)
     print(response.json())
-    response.raise_for_status()   
+    response.raise_for_status()
+    
+    
 
 
 def send_message(recipient_id, message_text):
@@ -298,10 +131,6 @@ def send_message(recipient_id, message_text):
 
 
 if __name__ == '__main__':
-    for category, id in CATEGORIES.items():
-        menu = json.dumps(get_products_by_category_id(SHOP_TOKEN, URL, id))
-        DATABASE.set(category, menu)
-    print('Бот стартует')
     host = os.environ['FLASK_HOST']
     port = os.environ['FLASK_PORT']
     app.run(host=host, port=port)
