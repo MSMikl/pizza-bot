f5dab5570c71110be198625b3bbf53f4f89426e3
MSMikl <77015283+MSMikl@users.noreply.github.com>
2022-09-22 10:54:24 +0300

    Global variables moved to app.config

--- webhook.py
+++ webhook.py
@@ -3,26 +3,14 @@ import os
 
 import redis
 import requests
+
+from apscheduler.schedulers.background import BackgroundScheduler
 from dotenv import load_dotenv
 from flask import Flask, request
 
 from shop import get_auth_token, get_file_link, get_products_by_category_id, add_item_to_cart, get_cart, delete_item
 
-load_dotenv()
-URL = 'https://api.moltin.com'
-SHOP_TOKEN, _ = get_auth_token(URL, os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET'))
-CATEGORIES = {
-    'front_page': '853639e2-b8de-41f8-99c5-cf26496e96f9',
-    'spicy': 'd83ce07a-d60c-44f6-936b-729339db5dab',
-    'nourishing': '2fcce823-9baa-4b7f-81dc-0030375ae27e',
-    'special': 'eb6b470c-91e9-49df-9e00-95826bdffe3f',    
-}
-DATABASE = redis.Redis(
-    host=os.getenv('DB_HOST'),
-    port=os.getenv('DB_PORT'),
-    password=os.getenv('DB_PASS'),
-    db=0    
-)
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -37,63 +25,18 @@ def verify():
 
     return "Hello world", 200
 
+@app.route('/moltin/', methods=['POST'])
+def update_menu():
+    if not request.headers.get('X-Moltin-Secret-Key') == os.environ['FB_VERIFY_TOKEN']:
+        return 'Bad request', 400
+    for category, id in app.config['APP_CATEGORIES'].items():
+        menu = json.dumps(get_products_by_category_id(app.config['APP_SHOP_TOKEN'], app.config['APP_URL'], id))
+    app.config['APP_DATABASE'].set(category, menu)
+    return "Menu updated", 201        
 
-def handle_start(sender_id, message_text, postback):
-    send_menu(sender_id)
+def handle_start(sender_id, message_text, postback, app_params):
+    send_menu(sender_id, app_params)
     return "MENU_AWAITING"
-
-
-def handle_menu(sender_id, message_text, postback):
-    if postback:
-        if postback['payload'] in CATEGORIES.keys():
-            send_menu(sender_id, postback['payload'])
-            return 'MENU_AWAITING'
-        elif postback['title'] == 'Корзина':
-            cart = get_cart(SHOP_TOKEN, URL, sender_id)
-            send_cart(sender_id, cart)
-            return 'CART'
-        elif postback['title'] == 'В корзину':
-            add_item_to_cart(SHOP_TOKEN, URL, sender_id, postback['payload'], 1)
-            send_message(sender_id, "Добавили в заказ")
-            return 'MENU_AWAITING'
-    return 'MENU_AWAITING'
-
-
-def handle_cart(sender_id, message_text, postback):
-    if not postback:
-        return 'CART'
-    if postback['title'] == 'Добавить еще одну':
-        cart = add_item_to_cart(SHOP_TOKEN, URL, sender_id, postback['payload'], 1)
-        send_cart(sender_id, cart)
-        return 'CART'
-    elif postback['title'] == 'Убрать из заказа':
-        cart = delete_item(SHOP_TOKEN, URL, sender_id, postback['payload'])
-        send_cart(sender_id, cart)
-        return 'CART'
-    elif postback['title'] == 'К меню':
-        send_menu(sender_id)
-        return 'MENU_AWAITING'
-    else:
-        return 'CART'
-
-
-def handle_users_reply(sender_id, message_text, postback):
-    states_functions = {
-        'START': handle_start,
-        'MENU_AWAITING': handle_menu,
-        'CART': handle_cart,
-    }
-    recorded_state = DATABASE.get(f"fb_{sender_id}")
-    if not recorded_state or recorded_state.decode('utf-8') not in states_functions.keys():
-        user_state = "START"
-    else:
-        user_state = recorded_state.decode('utf-8')
-    if message_text == "/start":
-        user_state = "START"
-    state_handler = states_functions[user_state]
-    next_state = state_handler(sender_id, message_text, postback)
-    DATABASE.set(f"fb_{sender_id}", next_state.encode('utf-8'))
-
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -101,31 +44,79 @@ def webhook():
     Основной вебхук, на который будут приходить сообщения от Facebook.
     """
     data = request.get_json()
-    message_text=None
-    postback=None
-    if data["object"] == "page":
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
-                if messaging_event.get("message"):  # someone sent us a message
-                    sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"] # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event['message']["text"] # the message's text
-                    handle_users_reply(sender_id, message_text, postback)
-                elif messaging_event.get('postback'):
-                    sender_id = messaging_event["sender"]["id"]
-                    postback = messaging_event.get('postback')
+    message_text = None
+    postback = None
+    if not data["object"] == "page":
+        return "ok", 200
+    for entry in data["entry"]:
+        for messaging_event in entry["messaging"]:
+            if messaging_event.get("message"):  # someone sent us a message
+                sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
+                recipient_id = messaging_event["recipient"]["id"] # the recipient's ID, which should be your page's facebook ID
+                message_text = messaging_event['message']["text"] # the message's text
+                handle_users_reply(sender_id, message_text, postback, app.config.get_namespace('APP_'))
+            elif messaging_event.get('postback'):
+                sender_id = messaging_event["sender"]["id"]
+                postback = messaging_event.get('postback')
 
-                    handle_users_reply(sender_id, message_text, postback)
+                handle_users_reply(sender_id, message_text, postback, app.config.get_namespace('APP_'))
     return "ok", 200
 
+def handle_menu(sender_id, message_text, postback, app_params):
+    if postback:
+        if postback['payload'] in app_params['categories'].keys():
+            send_menu(sender_id, app_params, postback['payload'])
+            return 'MENU_AWAITING'
+        elif postback['title'] == 'Корзина':
+            cart = get_cart(app_params['shop_token'], app_params['url'], sender_id)
+            send_cart(sender_id, cart)
+            return 'CART'
+        elif postback['title'] == 'В корзину':
+            add_item_to_cart(app_params['shop_token'], app_params['url'], sender_id, postback['payload'], 1)
+            send_message(sender_id, "Добавили в заказ")
+            return 'MENU_AWAITING'
+    return 'MENU_AWAITING'
 
-def send_menu(recipient_id, category='front_page'):
-    products = json.loads(DATABASE.get(category))['data']
-    # get_products_by_category_id(SHOP_TOKEN, URL, CATEGORIES[category])['data']
+def handle_cart(sender_id, message_text, postback, app_params):
+    if not postback:
+        return 'CART'
+    if postback['title'] == 'Добавить еще одну':
+        cart = add_item_to_cart(app_params['shop_token'], app_params['url'], sender_id, postback['payload'], 1)
+        send_cart(sender_id, cart)
+        return 'CART'
+    elif postback['title'] == 'Убрать из заказа':
+        cart = delete_item(app_params['shop_token'], app_params['url'], sender_id, postback['payload'])
+        send_cart(sender_id, cart)
+        return 'CART'
+    elif postback['title'] == 'К меню':
+        send_menu(sender_id, app_params)
+        return 'MENU_AWAITING'
+    else:
+        return 'CART'
+
+def handle_users_reply(sender_id, message_text, postback, app_params):
+    states_functions = {
+        'START': handle_start,
+        'MENU_AWAITING': handle_menu,
+        'CART': handle_cart,
+    }
+    recorded_state = app_params['database'].get(f"fb_{sender_id}")
+    if not recorded_state or recorded_state.decode('utf-8') not in states_functions.keys():
+        user_state = "START"
+    else:
+        user_state = recorded_state.decode('utf-8')
+    if message_text == "/start":
+        user_state = "START"
+    state_handler = states_functions[user_state]
+    next_state = state_handler(sender_id, message_text, postback, app_params)
+    app_params['database'].set(f"fb_{sender_id}", next_state.encode('utf-8'))
+
+def send_menu(recipient_id, app_params, category='front_page'):
+    products = json.loads(app_params['database'].get(category))['data']
     http_proxy = os.environ['HTTP_PROXY']
     proxies = { 
-              "http": http_proxy,
-              "https": http_proxy,
+            "http": http_proxy,
+            "https": http_proxy,
             }
     params = {"access_token": os.environ["FB_PAGE_ACCESS_TOKEN"]}
     headers = {"Content-Type": "application/json"}
@@ -165,7 +156,7 @@ def send_menu(recipient_id, category='front_page'):
                         {
                             "title": f"{product['name']} {product['price'][0]['amount']} р.",
                             "subtitle": product['description'],
-                            "image_url": get_file_link(SHOP_TOKEN, URL, product['relationships']['main_image']['data']['id']),
+                            "image_url": get_file_link(app_params['shop_token'], app_params['url'], product['relationships']['main_image']['data']['id']),
                             "buttons": [
                                 {
                                     "type": "postback",
@@ -206,12 +197,11 @@ def send_menu(recipient_id, category='front_page'):
     response = requests.post("https://graph.facebook.com/v14.0/me/messages", params=params, headers=headers, json=request_content, proxies=proxies)
     response.raise_for_status()   
 
-
 def send_cart(recipient_id, cart):
     http_proxy = os.environ['HTTP_PROXY']
     proxies = { 
-              "http": http_proxy,
-              "https": http_proxy,
+            "http": http_proxy,
+            "https": http_proxy,
             }
     params = {"access_token": os.environ["FB_PAGE_ACCESS_TOKEN"]}
     headers = {"Content-Type": "application/json"}
@@ -271,14 +261,13 @@ def send_cart(recipient_id, cart):
         }
     }
     response = requests.post("https://graph.facebook.com/v14.0/me/messages", params=params, headers=headers, json=request_content, proxies=proxies)
-    response.raise_for_status()   
-
+    response.raise_for_status()
 
 def send_message(recipient_id, message_text):
     http_proxy = os.environ['HTTP_PROXY']
     proxies = { 
-              "http": http_proxy,
-              "https": http_proxy,
+            "http": http_proxy,
+            "https": http_proxy,
             }
     params = {"access_token": os.environ["FB_PAGE_ACCESS_TOKEN"]}
     headers = {"Content-Type": "application/json"}
@@ -293,11 +282,76 @@ def send_message(recipient_id, message_text):
     response = requests.post("https://graph.facebook.com/v14.0/me/messages", params=params, headers=headers, json=request_content, proxies=proxies)
     response.raise_for_status()
 
+def set_moltin_webhook(url, shop_token):
+    headers = {
+        'Authorization': shop_token,
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'data': {
+            "type": "integration",
+            "name": "Product change notification",
+            "description": "Send message about menu updating",
+            "enabled": True,
+            "observes": [
+                "product.created",
+                'product.updated',
+                'product.deleted',
+            ],
+            "integration_type": "webhook",
+            "configuration": {
+                "url": "https://fb.michalbl4.ru/moltin/",
+                "secret_key": os.environ['FB_VERIFY_TOKEN'],
+            }
+        }
+    }
+    response = requests.post(
+        f"{url}/v2/integrations",
+        headers=headers,
+        json=data,
+    )
+    response.raise_for_status()
 
-if __name__ == '__main__':
-    for category, id in CATEGORIES.items():
-        menu = json.dumps(get_products_by_category_id(SHOP_TOKEN, URL, id))
-        DATABASE.set(category, menu)
+def refresh_token():
+    app.config['APP_SHOP_TOKEN'], _ = get_auth_token(app.config['APP_URL'], os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET'))
+
+
+def main():
+    load_dotenv()
+    url = 'https://api.moltin.com'
+
+    shop_token, _ = get_auth_token(url, os.getenv('CLIENT_ID'), os.getenv('CLIENT_SECRET'))
+    categories = {
+        'front_page': '853639e2-b8de-41f8-99c5-cf26496e96f9',
+        'spicy': 'd83ce07a-d60c-44f6-936b-729339db5dab',
+        'nourishing': '2fcce823-9baa-4b7f-81dc-0030375ae27e',
+        'special': 'eb6b470c-91e9-49df-9e00-95826bdffe3f',    
+    }
+    database = redis.Redis(
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        password=os.getenv('DB_PASS'),
+        db=0    
+    )
+    for category, id in categories.items():
+        menu = json.dumps(get_products_by_category_id(shop_token, url, id))
+        database.set(category, menu)
+    set_moltin_webhook(url, shop_token)
+    
+    app.config.update(
+        APP_SHOP_TOKEN = shop_token,
+        APP_URL = url,
+        APP_CATEGORIES = categories,
+        APP_DATABASE = database,
+    )
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(refresh_token, trigger="interval", seconds=3600)
+
     host = os.environ['FLASK_HOST']
     port = os.environ['FLASK_PORT']
     app.run(host=host, port=port)
+
+
+if __name__ == '__main__':
+    main()
